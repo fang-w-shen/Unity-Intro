f17bd3d17fef910e2c2c222295111b6884801e9f
Fang <fang-wen.shen@vanderbilt.edu>
2018-01-11 19:53:25 -0600

    finished basic collision detection and score count in C#

--- RollerBall/Assets/Scripts/PlayerController.cs
+++ RollerBall/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 public class PlayerController : MonoBehaviour {
 
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void Update () {
 		
 	}
 
     void FixedUpdate()
     {
-      
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speed);
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You've Won!";
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            Debug.Log(count);
+            SetCountText();
+        }
     }
 }
